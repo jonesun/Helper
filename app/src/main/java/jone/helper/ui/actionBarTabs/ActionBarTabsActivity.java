ac89d5ce2f5d560f41356c2a579aa7cab7d4a438
jone.sun <sunjoner7@gmail.com>
2015-01-20 18:03:22 +0800

    文件整理

--- app/src/main/java/jone/helper/ui/actionBarTabs/ActionBarTabsActivity.java
+++ app/src/main/java/jone/helper/ui/actionBarTabs/ActionBarTabsActivity.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -144,6 +146,27 @@ public class ActionBarTabsActivity extends Activity implements ActionBar.TabList
             }
             return null;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.action_settings:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
